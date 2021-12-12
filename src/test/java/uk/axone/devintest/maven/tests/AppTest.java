6d2e70afc3f3d895fcfaaf85ff44f54d21e93d0d
axoneuk <info@axone-tech.uk>
2021-12-12 08:25:18 +0000

    Adding to GitHub Repo

---
+++ src/test/java/uk/axone/devintest/maven/tests/AppTest.java
@@ -0,0 +1,28 @@
+package uk.axone.devintest.maven.tests;
+
+import org.junit.Test;
+
+
+public class AppTest {
+
+    @Test
+    public void test1(){
+        System.out.println("Running test 1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Running test 1");
+    }
+
+    @Test
+    public void test3(){
+        System.out.println("Running test 1");
+    }
+
+    @Test
+    public void test4(){
+        System.out.println("Running test 1");
+    }
+
+}
